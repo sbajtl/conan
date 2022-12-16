2b76b0d439661f86e112014a4187e6b9cb9a99a9
sasabajtl <sasabajtl@sprend.com>
2022-12-16 15:35:21 +0100

    Data

---
+++ toolchains/macos/android_arm64-v8a.cmake
@@ -0,0 +1,3 @@
+set(ANDROID_PLATFORM 24)
+set(ANDROID_ABI arm64-v8a)
+include($ENV{HOME}/Library/Android/sdk/ndk/25.0.8775105/build/cmake/android.toolchain.cmake)
---
+++ toolchains/macos/android_x86_64.cmake
@@ -0,0 +1,3 @@
+set(ANDROID_PLATFORM 24)
+set(ANDROID_ABI x86_64)
+include($ENV{HOME}/Library/Android/sdk/ndk/25.0.8775105/build/cmake/android.toolchain.cmake)
