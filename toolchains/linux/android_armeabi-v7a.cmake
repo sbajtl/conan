3420135f087e2d7fa5b052b8ab11151ab9f39753
sbajtl <sasabajtl@gmail.com>
2022-12-18 20:20:54 +0100

    Toolchain files

--- toolchains/linux/android_armeabi-v7a.cmake
+++ toolchains/linux/android_armeabi-v7a.cmake
@@ -1,3 +1,3 @@
 set(ANDROID_PLATFORM 24)
 set(ANDROID_ABI armeabi-v7a)
-include(~/Android/sdk/ndk/25.1.8937393/build/cmake/android.toolchain.cmake)
+include(~/Android/Sdk/ndk/25.1.8937393/build/cmake/android.toolchain.cmake)
--- toolchains/linux/android_x86_64.cmake
+++ toolchains/linux/android_x86_64.cmake
@@ -1,3 +1,3 @@
 set(ANDROID_PLATFORM 24)
 set(ANDROID_ABI x86_64)
-include(~/Android/sdk/ndk/25.1.8937393/build/cmake/android.toolchain.cmake)
+include(~/Android/Sdk/ndk/25.1.8937393/build/cmake/android.toolchain.cmake)
