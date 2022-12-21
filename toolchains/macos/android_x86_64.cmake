91f5630377628835710deaa6bcf4127162c81d2e
Saša Bajtl <sbajtl@Sasas-iMac-Pro.local>
2022-12-21 14:54:48 +0100

    Update ndk version to latest.

--- toolchains/macos/android_x86_64.cmake
+++ toolchains/macos/android_x86_64.cmake
@@ -1,3 +1,3 @@
 set(ANDROID_PLATFORM 24)
 set(ANDROID_ABI x86_64)
-include($ENV{HOME}/Library/Android/sdk/ndk/25.0.8775105/build/cmake/android.toolchain.cmake)
+include($ENV{HOME}/Library/Android/sdk/ndk/25.1.8937393/build/cmake/android.toolchain.cmake)
